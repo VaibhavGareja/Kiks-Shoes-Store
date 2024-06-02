fe2b515034bbc066eb33e9d51a83d24b6dc2e02a
Vaibhav Gareja <gvkstudy1010@gmail.com>
2024-06-02 15:57:12 +0530

    env

--- Server/routes/productRoute.js
+++ Server/routes/productRoute.js
@@ -5,39 +5,42 @@ const { fetchAllProducts, checkOutPayment } = require("../controller/product");
 router.get("/products", fetchAllProducts);
 
 router.post("/create-checkout-session", checkOutPayment);
-const endpointSecret = "whsec_d5cb3bcfe9feb50a5dbf52a4c7b9ea1173f3ec639591d1a01f66012dc80a8864";
+const endpointSecret = process.env.ENDPOINTSECRER;
 
-router.post('/webhook', express.raw({type: 'application/json'}), (request, response) => {
-  const sig = request.headers['stripe-signature'];
+router.post(
+  "/webhook",
+  express.raw({ type: "application/json" }),
+  (request, response) => {
+    const sig = request.headers["stripe-signature"];
 
-  let event;
+    let event;
 
-  try {
-    event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
-  } catch (err) {
-    response.status(400).send(`Webhook Error: ${err.message}`);
-    return;
+    try {
+      event = stripe.webhooks.constructEvent(request.body, sig, endpointSecret);
+    } catch (err) {
+      response.status(400).send(`Webhook Error: ${err.message}`);
+      return;
+    }
+
+    // Handle the event
+    switch (event.type) {
+      case "payment_intent.succeeded":
+        const paymentIntentSucceeded = event.data.object;
+        break;
+      case "payment_intent.requires_action":
+        const paymentIntentRequiresAction = event.data.object;
+        break;
+      case "payment_intent.created":
+        const paymentIntentCreated = event.data.object;
+        break;
+      default:
+        console.log(`Unhandled event type ${event.type}`);
+    }
+
+    // Return a 200 response to acknowledge receipt of the event
+    response.send();
   }
-
-  // Handle the event
-  switch (event.type) {
-    case 'payment_intent.succeeded':
-      const paymentIntentSucceeded = event.data.object;
-      break;
-    case 'payment_intent.requires_action':
-      const paymentIntentRequiresAction = event.data.object;
-      break;
-    case 'payment_intent.created':
-      const paymentIntentCreated = event.data.object;
-      break;
-    default:
-      console.log(`Unhandled event type ${event.type}`);
-  }
-  
-
-  // Return a 200 response to acknowledge receipt of the event
-  response.send();
-});
+);
 
 // let endpointSecret;
 
