1c2f9c9705da01f5ee185526ccbb2af02c488bd7
Vaibhav Gareja <gvkstudy1010@gmail.com>
2024-07-14 20:00:09 +0530

    setup build

--- Server/server.js
+++ Server/server.js
@@ -1,35 +1,26 @@
 const express = require("express");
 const cors = require("cors");
-require("dotenv").config()
-dotenv.config();
+const dotenv = require("dotenv"); // Import dotenv module
+const path = require("path"); // Import path module
 const app = express();
 const db = require("./helper/db");
 const PORT = process.env.PORT;
 const bodyParser = require('body-parser');
-const productRoute = require("./routes/productRoute")
-const userRoute = require("./routes/userRoute")
+const productRoute = require("./routes/productRoute");
+const userRoute = require("./routes/userRoute");
 const Product = require("./model/productSchema");
-const productData = require("./topProducts.json")
+const productData = require("./topProducts.json");
+
+dotenv.config(); // Load environment variables from .env file
 
 app.use(express.json());
 app.use(express.static(path.join(__dirname, '../Client/dist')));
-app.use(bodyParser.raw({type: 'application/json'}));
+app.use(bodyParser.raw({ type: 'application/json' }));
 app.use(cors());
 
-app.use("/",productRoute)
-app.use("/auth",userRoute)
+app.use("/", productRoute);
+app.use("/auth", userRoute);
 
-// (async () => {
-//   try {
-//     for (const product of productData.shoes) { 
-//       const newProduct = new Product(product); 
-//       await newProduct.save();
-//     }
-//     console.log("Products inserted into the database successfully");
-//   } catch (err) {
-//     console.error("Error inserting products into the database:", err);
-//   }
-// })();
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '../Client/dist', 'index.html'));
 });
